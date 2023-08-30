b8de83bc9abcb525623ddabc4d6066df912135de
Hello - WEI <35485370+addpd@users.noreply.github.com>
2023-08-30 19:35:28 +0800

    Update PageHead.tsx
    JSX 元素“meta” 添加相应的结束标记

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -24,7 +24,7 @@ export const PageHead: React.FC<
     <Head>
       <meta charSet='utf-8' />
       <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       <meta
         name='viewport'
         content='width=device-width, initial-scale=1, shrink-to-fit=no'
